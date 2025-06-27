1d31822179004a0c08484e4a00af91eed887a9eb
hendraaagil <hendraaagil@gmail.com>
2025-06-27 14:26:23 +0700

    feat: add valorant route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import { Hono } from 'hono'
+import { fetchValorantProfile } from './libs/fetcher'
 
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello from StreamKit!')
+})
+
+app.get('/valorant', async (c) => {
+  const username = c.req.query('username') ?? ''
+  const data = await fetchValorantProfile(username)
+  if (!data) {
+    return c.text('Failed to fetch data')
+  }
+
+  const decodedUsername = decodeURIComponent(username)
+  const rank = data.rank.displayValue || 'Unranked'
+  const currentRank = rank + (rank.value ? ` (${rank.value})` : '')
+  const peakRank = data.peakRank.displayValue || 'Unranked'
+  const peakAct = data.peakRank.displayValue
+    ? `(${data.peakRank.metadata.actName})`
+    : ''
+
+  return c.text(
+    `${decodedUsername} - ${currentRank} | Peak Rank: ${peakRank} ${peakAct}`,
+  )
 })
 
 export default app
